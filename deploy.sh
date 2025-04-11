5c192a549da9d49c5fc48db31c94874ac4eeb98d
Peter Huang <miterfrnats@gmail.com>
2025-04-11 14:21:06 +0800

    devops: 更新 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git checkout -f;
 git fetch --all;
-git pull origin staging;
+git pull origin main;
 
 docker build -t cbct-admin-api ./ --rm --no-cache
 
@@ -18,3 +18,16 @@ docker run -d \
 -v /var/project/CBCT/AdminApi/secrets.json:/app/secrets.json \
 -v /var/project/CBCT/data:/data \
 cbct-admin-api
+
+
+if [ "$(sudo docker ps -q -f name=cbct-admin-f2e)" ]; then
+   sudo docker stop cbct-admin-f2e
+fi
+if [ "$(sudo docker container ls -f name=cbct-admin-f2e)" ]; then
+   sudo docker rm cbct-admin-f2e
+fi
+
+docker run -d \
+-p 8080:80 \
+--name cbct-admin-f2e \
+cbct-admin-f2e
